ea7b61caf2075be42930bf6a0f1ce7397ff49a78
MonsterEVO <mandel.js@gmail.com>
2016-04-07 08:58:08 -0700

    Update CVEImport.py
    Now adds all cvss info

--- CVEImport.py
+++ CVEImport.py
@@ -3,7 +3,7 @@ import gzip
 import os
 from xml.dom import minidom
 import psycopg2 as dbapi2
-import time
+
 
 
 #varibles
@@ -46,10 +46,29 @@ for nodeEntry in xmldoc.getElementsByTagName("entry"):                          
     lastmod = nodeEntry.getElementsByTagName("vuln:last-modified-datetime")[0]
     modvalue = lastmod.childNodes[0].data[:10]
     for nodeSoftware in software:                                                                                       #for loop to get vulnerable sw readable data
+        score = nodeEntry.getElementsByTagName("cvss:score")[0]
+        scorevalue = score.childNodes[0].data
+        accvec = nodeEntry.getElementsByTagName("cvss:access-vector")[0]
+        accvecval = accvec.childNodes[0].data
+        acccomp = nodeEntry.getElementsByTagName("cvss:access-complexity")[0]
+        acccompval = acccomp.childNodes[0].data
+        authreq = nodeEntry.getElementsByTagName("cvss:authentication")[0]
+        authreqval = authreq.childNodes[0].data
+        confimp = nodeEntry.getElementsByTagName("cvss:confidentiality-impact")[0]
+        confimpval = confimp.childNodes[0].data
+        intimp = nodeEntry.getElementsByTagName("cvss:integrity-impact")[0]
+        intimpval = intimp.childNodes[0].data
+        availimp = nodeEntry.getElementsByTagName("cvss:availability-impact")[0]
+        availimpval = availimp.childNodes[0].data
+        summary = nodeEntry.getElementsByTagName("vuln:summary")[0]
+        summaryval = summary.childNodes[0].data
         vulnsw = nodeSoftware.childNodes[0].data
         SQLstr = vulnsw[7:]  # Strips off prefix
-        SQLinsert = "INSERT INTO cve_list (cve_id, software_package, pub_date, mod_date) VALUES (%s, %s, %s, %s)"  # Defines SQL insert function to be used
-        cur.execute(SQLinsert, (entryid, SQLstr, pubvalue, modvalue))  # Insert data  #Executes SQL statement in postgres
+        SQLinsert = "INSERT INTO cve_list (cve_id, software_package, pub_date, mod_date, score, access_vector, \
+        access_complexity, authen, conf_impact, integ_impact, avail_impact, summary) VALUES \
+        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"  # Defines SQL insert function to be used
+        cur.execute(SQLinsert, (entryid, SQLstr, pubvalue, modvalue, scorevalue, accvecval, acccompval, authreqval,
+                                confimpval, intimpval, availimpval, summaryval))  # Insert data  #Executes SQL statement in postgres
         conn.commit()
 
 #Create Reference Table
