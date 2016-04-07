8b54ad00c5b2676057ff7ae5981a684abf7e480c
hensojr <hensojr@gmail.com>
2016-04-07 14:39:58 -0600

    Added files via upload

--- GUI.py
+++ GUI.py
@@ -11,9 +11,10 @@ conn = dbapi2.connect (database="CVE", host="localhost", user="postgres", passwo
 xmldoc = minidom.parse("test.xml")
 xmlfile = "test.xml"
 
-#Establish a cursor
+#establish a cursor
 cur = conn.cursor()
 
+#create gui
 class App(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self,master)
@@ -21,57 +22,60 @@ class App(tk.Frame):
         self.grid()
         self.ichose = []
 
-        self.l = Listbox(self, height=20, width=100, selectmode=MULTIPLE)
-        # Selectmode can be SINGLE, BROWSE, MULTIPLE or EXTENDED. Default BROWSE
-        self.l.grid(column=0, row=0, sticky=(N,W,E,S))
+        #creates listbox for software
+        self.listB = Listbox(self, height=20, width=80, selectmode=MULTIPLE)
+        self.listB.grid(row=1, sticky=(N,W))
 
-        s = Scrollbar(self, orient=HORIZONTAL, command=self.l.yview)
-        s.grid(column=0, row=0, sticky=(N,S,E))
-        self.l['yscrollcommand'] = s.set
+        #creates scrollbar
+        scrollB = Scrollbar(self, orient=HORIZONTAL, command=self.listB.yview)
+        scrollB.grid(column=0, row=0, sticky=(N,E))
+        self.listB['yscrollcommand'] = scrollB.set
 
-        software = xmldoc.getElementsByTagName("vuln:product")
+        #creates submit button
+        submitB = Button(self, text='Submit', fg='red', command=self.quit)
+        submitB.grid(column=0, row=13)
 
-        #for nodeSoftware in software:
-         #   vulnsw = nodeSoftware.childNodes[0].data
+        # creates system name entry field and label
+        eLabel= Label(master, text='System Name').grid(column=0, row=15)
+        userE = Entry(self, fg='black').grid(column=0, row=14)
 
+
+        #pulls software list from db to add to listbox
         cur.execute("SELECT software_name FROM software_list;")
         for row in cur:
             (cur.fetchone())
-            self.l.insert('end', row)
+            self.listB.insert('end', row)
 
+        # create textbox that will display selected items from list
+        self.selected_list = Text(self,width=91, height=20,wrap=WORD)
+        self.selected_list.grid(row=2, column=0, sticky=(S,W))
 
-            # appends multiple SW values to CVE key
-
-
-        #for i in range(1,101):
-         #   self.l.insert('end', 'Line %d of 100' % i)
-
-        # Create Textbox that will display selected items from list
-        self.selected_list = Text(self,width=50, height=10,wrap=WORD)
-        self.selected_list.grid(row=12, column=0, sticky=W)
-
-        # Now execute the poll() function to capture selected list items
+        # executes the poll() function to capture selected list items
         self.ichose = self.poll()
 
     def poll(self):
         items =[]
         self.ichose = []
-        # Set up an automatically recurring event that repeats after 200 millisecs
-        self.selected_list.after(200, self.poll)
-        # curselection retrieves the selected items as a tuple of strings. These
-        # strings are the list indexes ('0' to whatever) of the items selected.
-        # map applies the function specified in the 1st parameter to every item
-        # from the 2nd parameter and returns a list of the results. So "items"
-        # is now a list of integers
-        items = map(int,self.l.curselection())
 
-        # For however many values there are in "items":
+        # set up an automatically recurring event that repeats after 200 millisecs
+        # curselection retrieves the selected items as a tuple of strings
+        # strings are the list indexes ('0' to whatever) of the items selected.
+        self.selected_list.after(200, self.poll)
+
+
+        # map applies the function specified in the 1st parameter to every item
+        # from the 2nd parameter and returns a list of the results so "items"
+        # is now a list of integers
+        items = map(int,self.listB.curselection())
+
+        # for however many values there are in "items"
+        # use each number as an index and get from the listbox the actual
+        # text strings corresponding to each index, and append each to
         for i in range(len(items)):
-            # Use each number as an index and get from the listbox the actual
-            # text strings corresponding to each index, and append each to
             # the list "ichose".
-            self.ichose.append(self.l.get(items[i]))
-        # Write ichose to the textbox to display it.
+            self.ichose.append(self.listB.get(items[i]))
+
+        # write ichose to the textbox to display it
         self.update_list()
         return self.ichose
 
@@ -79,13 +83,15 @@ class App(tk.Frame):
         self.selected_list.delete(0.0, END)
         self.selected_list.insert(0.0, self.ichose)
 
-
-
-
-root=tk.Tk()
-root.title('Relevant Software Selection')
-root.geometry('500x1000')
-app=App(root)
-root.mainloop()
+cveGui=tk.Tk()
+cveGui.title('Relevant Software Selection')
+cveGui.geometry('1280x800+200+200')
+app=App(cveGui)
+cveGui.mainloop()
 
 print app.ichose
+
+sqlinsert = "INSERT INTO system_software (software_name) VALUES (%s)"
+cur.execute(sqlinsert, [app.ichose])
+conn.commit()
+
